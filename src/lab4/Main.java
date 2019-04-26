8283670300e34513f35cc26e3f76f55ba8e1fd05
HanhND <hanhndth1808055@fpt.edu.vn>
2019-04-26 21:19:06 +0700

    Lab 4

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String args[]) {
         Room room = new Room();
         room.addRoom(room);
-        room.addUser(5, room);
+        room.addUser(room);
         room.printRoom(room);
         room.deleteUser();
         room.printRoom(room);
--- src/lab4/Room.java
+++ src/lab4/Room.java
@@ -1,12 +1,13 @@
 package lab4;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Room {
     public String name;
     public String location;
-    public ArrayList<String> userList;
+    public ArrayList<User> userList;
 
     public Room() {
         this.userList = new ArrayList<>();
@@ -17,36 +18,77 @@ public class Room {
         this.location = location;
         this.userList = new ArrayList<>();
     }
-public void addRoom(Room room){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Please insert room's name: ");
-    room.name = scanner.nextLine();
-//        System.out.println(room.name);
-    System.out.println("Please insert room's location: ");
-    room.location = scanner.nextLine();
 
-}
-    public void addUser(int n, Room room) {
+    public void addRoom(Room room) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
-            System.out.println("Please insert room's " + " user number " + (i + 1) + ": ");
-            room.userList.add(scanner.nextLine());
-        }
+        System.out.println("Please insert room's name: ");
+        room.name = scanner.nextLine();
+//        System.out.println(room.name);
+        System.out.println("Please insert room's location: ");
+        room.location = scanner.nextLine();
+
     }
+
+    public void addUser(Room room) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert user's id");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please insert user's name");
+        String name = scanner.nextLine();
+
+        System.out.println("Please insert user's email");
+        String email = scanner.nextLine();
+
+        System.out.println("Please insert user's password");
+        String password = scanner.nextLine();
+
+        System.out.println("Please insert user's gallery");
+        ArrayList<String> stringList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please insert URL of picture: ");
+            stringList.add(scanner.nextLine());
+        }
+
+        User user = new User(id, name, email, password);
+        user.gallery = stringList;
+
+        userList.add(user);
+
+    }
+
 
     public void printRoom(Room room) {
         System.out.println("Room's name: " + room.name);
         System.out.println("Room's location: " + room.location);
         System.out.println("Room's user list: ");
-        for (String u : userList) {
-            System.out.println(u);
+        for (User u : userList) {
+            System.out.println("ID: " + u.id);
+            System.out.println("Name: " + u.name);
+            System.out.println("Email: " + u.email);
+            System.out.println("Password: " + u.password);
+
+            System.out.println("~~~~~~~~~~~~~");
         }
     }
-    public void deleteUser(){
+
+    public void deleteUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insert index to delete user: ");
-        int a = scanner.nextInt();
-        userList.remove(a);
+        System.out.println("Insert id to delete user: ");
+        int id = scanner.nextInt();
+        for (int i = 0; i < userList.size(); i++){
+            if(userList.get(i).id == id){
+                userList.remove(i);
+            }
+        }
+//        System.out.println("Insert name to delete user: ");
+//        String name = scanner.nextLine();
+//        for (int i = 0; i < userList.size(); i++){
+//            if(userList.get(i).name.equals(name)){
+//                userList.remove(i);
+//            }
+//        }
+////        userList.remove(a);
     }
 }
 
